d96671bbddadd43593b8e9733398a7827788d9f3
Sondre Halvorsen <sondre@sonhal.no>
2023-10-17 21:21:36 +0200

    Skeleton ideas

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 ### Implant
 
 ### Server
+
+
+- [] Receive commands from user (client)
+- [] Send and receive commands and data from impant
+    - [] Support HTTP
+    - [] Support DNS
+- 
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+// Setup state to hold implant connection info
+// handle implant communication
+    // Implant register request (new implant calls home)
+    // Communication channel with existing implants (queue based?)
+
+
+struct WaspConnection {
+    name: String
+}
+
+
+struct WaspHive {
+    name: String,
+    wasps: Vec<WaspConnection>
+}
+
+
+impl  WaspHive {
+
+    fn handle(register: String) {
+        todo!()
+    }
+
+    fn 
+    
+}
